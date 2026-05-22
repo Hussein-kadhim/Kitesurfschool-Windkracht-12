85823140efcaa2594d223ee54ac1cc029f71fd26
Hussein-kadhim <h-kadhim@outlook.com>
2026-05-22 10:37:30 +0200

    chore: basis project structuur - backend, frontend config en gedeelde componenten

---
+++ Frontend/src/components/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='text-red-500 mb-4 mt-4 font-bold text-2xl flex items-center justify-center h-screen'>404 - Pagina Niet Gevonden</div>
+  )
+}
+
+export default NotFound
